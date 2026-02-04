fd334ae75c90db387082e8a0360eb027e12438db
Joel Benton <joelbenton1903@Gmail.com>
2026-02-04 15:56:18 +0000

    Smith Gloves patch is now executed

--- AldravaineRaces/AldravaineRaces/src/Patches/SmithGloves.cs
+++ AldravaineRaces/AldravaineRaces/src/Patches/SmithGloves.cs
@@ -13,18 +13,12 @@ namespace AldravaineRaces.src.Patches
         [HarmonyPrefix]
         public static bool Gear_Has_Heat_Resistant(ItemSlot slot, IPlayer player)
         {
-            System.Diagnostics.Debug.WriteLine("Checking for heat resistant gloves...");
-            System.Diagnostics.Debug.WriteLine($"Slot Empty: {slot.Empty}, Player: {player?.PlayerName}, GameMode: {player?.WorldData.CurrentGameMode}");
             if (slot.Empty || player == null || player.WorldData.CurrentGameMode == EnumGameMode.Creative) return false;
-
-            System.Diagnostics.Debug.WriteLine("Player has gloves equipped, checking for heat resistance...");
-            System.Diagnostics.Debug.WriteLine($"Player Entity: {player.Entity}");
             if (player.Entity?
                     .GetBehavior<EntityBehaviorPlayerInventory>() is not { Inventory: not null } playerInventory)
                 return true;
             foreach (var itemSlot in playerInventory.Inventory)
             {
-                System.Diagnostics.Debug.WriteLine($"Checking slot: {itemSlot.SlotId}, BackgroundIcon: {itemSlot.BackgroundIcon}, Empty: {itemSlot.Empty}");
                 if (itemSlot.BackgroundIcon != "gloves")
                 {
                     continue;
