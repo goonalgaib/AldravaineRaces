6ca0fc4d71af955bb920307a2d5531d3d029b0bd
Joel Benton <joelbenton1903@Gmail.com>
2026-02-21 19:27:19 +0000

    Added Eternal Storm Patch.

---
+++ AldravaineRaces/AldravaineRaces/src/Patches/PlayerModelLib.cs
@@ -0,0 +1,74 @@
+﻿//using HarmonyLib;
+//using PlayerModelLib;
+//using System;
+//using System.Reflection;
+//using Vintagestory.API.Common;
+//using Vintagestory.GameContent;
+
+
+//namespace AldravaineRaces.src.Patches
+//{
+//    [HarmonyPatch]
+//    public class Patch_applyTraitAttributes
+//    {
+//        static MethodBase TargetMethod()
+//        {
+//            return AccessTools.Method(
+//                "PlayerModelLib.OtherPatches:applyTraitAttributes"
+//            );
+//        }
+
+//        static void Postfix(CharacterSystem __instance, EntityPlayer eplr)
+//        {
+//            //if (eplr == null || eplr.Api == null) return;
+
+//            //string name = eplr.Player?.PlayerName ?? eplr.GetName();
+//            //string classCode = eplr.WatchedAttributes?.GetString("characterClass", "<none>");
+
+
+
+//            //var api = eplr.Api;
+
+//            //var skinBehavior = eplr.GetBehavior<PlayerSkinBehavior>();
+//            //string modelCode = skinBehavior?.CurrentModelCode ?? "<null>";
+//            //var modelSystem = api.ModLoader.GetModSystem<CustomModelsSystem>();
+//            //if (modelCode == "<null>" || !modelSystem.CustomModels.TryGetValue(modelCode, out var model))
+//            //{
+//            //    api.Logger.Warning("[AldravaineRaces] No custom model found for code: " + modelCode);
+//            //    return;
+//            //}
+
+//            //string[] extraTraits = model.ExtraTraits;
+//            //foreach (string traitCode in extraTraits) {
+//            //    if (!__instance.TraitsByCode.TryGetValue(traitCode, out Trait? trait)) continue;
+
+//            //    foreach ((string attributeCode, double attributeValue) in trait.Attributes)
+//            //    {
+//            //        api.Logger.Debug($"[AldravaineRaces] Applying trait {traitCode} attribute {attributeCode} with value {attributeValue} to player {name} (class {classCode})");
+//            //    }
+//            //}
+//            //eplr.WatchedAttributes.SetStringArray("extraTraits", extraTraits);
+
+//            // Assuming eplr.Stats is your EntityStats object
+//            var stats = eplr.Stats;
+
+//            foreach (var categoryPair in stats) // categoryPair.Key = category name, categoryPair.Value = EntityFloatStats
+//            {
+//                string categoryName = categoryPair.Key;
+//                EntityFloatStats floatStats = categoryPair.Value;
+
+//                Console.WriteLine($"Category: {categoryName}, BlendType: {floatStats.BlendType}");
+
+//                foreach (var statPair in floatStats.ValuesByKey) // statPair.Key = code, statPair.Value = EntityStat<float>
+//                {
+//                    string code = statPair.Key;
+//                    float value = statPair.Value.Value;
+//                    bool persistent = statPair.Value.Persistent;
+
+//                    Console.WriteLine($"  {code} = {value} (Persistent: {persistent})");
+//                }
+//            }
+
+//        }
+//    }
+//}
---
+++ AldravaineRaces/AldravaineRaces/src/Patches/eternalstorm.cs
@@ -0,0 +1,44 @@
+﻿using canjewelry.src.be;
+using canjewelry.src.blocks;
+using canjewelry.src.jewelry;
+using EternalStorm;
+using HarmonyLib;
+using System;
+using System.Linq;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+using Vintagestory.Client;
+using Vintagestory.GameContent;
+using Vintagestory.ServerMods;
+
+namespace AldravaineRaces.src.Patches
+{
+    [HarmonyPatch(typeof(EternalStormModSystem), "Start")]
+    public class Patch_EternalStorm_Start
+    {
+        static void Postfix(
+            EternalStormModSystem __instance,
+            ref EternalStormModConfig ___config,
+            ref ICoreAPI ___api)
+        {
+            if (___config == null) return;
+
+            ___api?.Logger.Notification("EternalStorm config overridden.");
+
+            ___config.PlayerMaxSaturation = 5000f;
+            //___config.LowStabilityDamage = 0f;
+            //___config.LowStabilityHungerCost = 0f;
+            //___config.StabilityPerGearUse = 0f;
+            //___config.DamageOnTemporalGearUse = 0f;
+            //___config.RiftDamageRadius = 4f;
+            //___config.RiftDamagePerSecond = 3f;
+            ___config.BorderStart = 999;
+            ___config.BorderEnd = 1500;
+            //___config.BorderSanityPerSecond = 0.01;
+
+            ___api?.StoreModConfig(___config, EternalStormModSystem.ConfigName);
+        }
+    }
+}
